5022656b0824b850e8963f5b5d75fb22cfbd07bc
AndreaDeGiuli <93667724+AndreaDeGiuli@users.noreply.github.com>
2022-04-07 00:46:36 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,60 +1,55 @@
-from pickle import TRUE
-import ORM
-import Connection
-import psycopg2
+from ORM import Query 
+from Connection import MessagesServer, MessagesP2P
 import os
 
-def disponibilitaFile(md5):
-    presente = False
-    conn = psycopg2.connect("host=localhost dbname=db")
-    query="SELECT COUNT(*) FROM P2P_File where md5 = %e" %md5
-    cur = conn.cursor()
-    cur.execute(query)
-    if(cur.rowcount > 0):
-        presente = True
-    cur.close()
-    conn.close()
-    return presente
-   
-def login(IP, porta):
-    sessionID = os.random(16)
-    conn = psycopg2.connect("host=localhost dbname=db")
-    cur = conn.cursor()
-    query = "SELECT sessionID FROM P2P where sessionID = %e" %sessionID
-    cur.execute(query)
-    cur.fetchall()
-    if(cur.rowcount == 0):
-        query = "INSERT INTO P2P (sessionID, IP, porta) VALUES ('{%e}','{%e}','{%e}')" %sessionID %IP %porta
-        cur.execute(query)
-        return True, sessionID
-    sessionID = '0000000000000000'
-    return False, sessionID
+def login(ip, porta):
+    if(Query.PeerRegistrato(ip, porta)):
+        sessionID = '0000000000000000'
+        return False, sessionID
+    while(True):
+        sessionID = os.random(16)
+        if(not Query.SessionIDpresente(sessionID)):   
+            Query.EffettuaLogin(sessionID,ip, porta)
+            return True, sessionID
 
 def logout(sessionID): 
-    conn = psycopg2.connect("host=localhost dbname=db")
-    cur = conn.cursor()
-    query = "Select COUNT(*) FROM P2P_File where sessionID = %e" %sessionID
-    cur.execute(query)
-    mobile_records = cur.fetchall()
-    for row in mobile_records:
-        copy = row[0]
-    query = "Delete FROM P2P where sessionID = %e" %sessionID
-    cur.execute(query)
-    cur.fetchall()
-    if(cur.rowcount == 0):
-        query = "INSERT INTO P2P (sessionID, IP, porta) VALUES ('{%e}','{%e}','{%e}')" %sessionID %IP %porta
-        cur.execute(query)
-        return True, sessionID
-    sessionID = '0000000000000000' 
+    n_fileRimossi=Query.EffettuaLogout(sessionID)
+    return n_fileRimossi
 
-    
-def ricercaPerNome(nome):
-    conn = psycopg2.connect("host=localhost dbname=db")
-    cur = conn.cursor()
-    query = "SELECT md5,COUNT(md5) FROM P2P_File where nome = {%e} GROUP by md5" %nome
-    cur.execute(query)
-    mobile_records = cur.fetchall()
-    for row in mobile_records:
-        print("md5 = ", row[0], )
-        print("copy = ", row[1], "\n")
+def aggiungiFile(sessionId, md5, filename):
+    result = MessagesServer.QueryADDF(sessionId, md5, filename)
+    return result
+
+def rimuoviFile(sessionId, md5):
+    result = MessagesServer.QueryDELF(sessionId, md5)
+    return result
+
+def researchFile(sessionId, search_string):
+    result = MessagesServer.QueryFIND(sessionId, search_string)
+    return result
+
+parametri = MessagesServer.Read(s)
+messageHeader = parametri[0]
+if messageHeader == "LOGI":
+    result = login(parametri[1], parametri[2])
+    MessagesServer.LoginAnswer(s, result[1])
+elif messageHeader == "ADDF":
+    result = aggiungiFile(parametri[1], parametri[2], parametri[3])
+    MessagesServer.AddFileAnswer(s, result)
+elif messageHeader == "DELF":
+    result = MessagesServer.QueryDELF(parametri[1], parametri[2])
+    MessagesServer.RemoveFileAnswer(s, result)
+elif messageHeader == "FIND":
+    result = MessagesServer.QueryFIND(parametri[1], parametri[2])
+    files =[]
+    for i in range(0, len(result[0])):
+        f = File(result[0][i],result[1][i],result[2][i])
+        files.append(f)
+    MessagesServer.FindFileAnswer(s, files)
+elif messageHeader == "RREG":
+    result = MessagesServer.QueryRREG(parametri[2], parametri[3], parametri[4])
+    MessagesServer.DownloadAnswer(s, result)
+elif messageHeader == "LOGO":
+    result = logout(parametri[1])
+    MessagesServer.LogoutAnswer(s,result)
 
